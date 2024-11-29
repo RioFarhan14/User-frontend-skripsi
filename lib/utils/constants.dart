4001f8a39460524400fc6e97f12c156e3ad9ca26
RioFarhan14 <riofarhan383@gmail.com>
2024-11-29 22:53:13 +0700

    fix url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
-const String BASE_URL = 'http://resource.futsal-am.my.id';
+const String BASE_URL = 'http://resource.futsal-am.online:3000';
 const String MIDTRANS_CLIENT_KEY = "SB-Mid-client-pOBoAI7bwOPaEEjD";
 // const String MIDTRANS_MERCHANT_BASE_URL =
 //     "https://app.sandbox.midtrans.com/snap/snap.js";
